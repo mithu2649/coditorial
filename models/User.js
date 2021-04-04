54cccc457725c43d1a02ee6781891a680a43b3d0
Mithu Mondal <mithu2649@gmail.com>
2021-04-04 22:13:11 +0530

    updated user model: removed unused function

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const { isEmail, isAlphanumeric, isAlpha } = require('validator');
+const { isEmail, isAlphanumeric } = require('validator');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
